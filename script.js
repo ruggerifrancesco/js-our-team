64ae560025cc74cf6a22d48d070fac06788f14c3
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-29 16:15:38 +0200

    changed func to only name id, added new members

--- script.js
+++ script.js
@@ -11,29 +11,37 @@ console.log('JS LOADED!');
 
 
 // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// MILESTONE 0:
+// MILESTONE 0:  ✓
 // Creare l’array di oggetti con le informazioni fornite. ✓
-// MILESTONE 1:
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 1:  ✓
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto  ✓
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 const teamMembersList = [
-    addNewMember('Wayne', 'Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'),
-    addNewMember('Wayne', 'Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'),
-    addNewMember('Wayne', 'Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'),
-    addNewMember('Wayne', 'Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'),
+    addNewMember('Wayne Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'),
+    addNewMember('Angela Caroll', 'Chief Editor', 'angela-caroll-chief-editor.jpg'),
+    addNewMember('Walter Gordon', 'Office Manager', 'walter-gordon-office-manager.jpg'),
+    addNewMember('Angela Lopez', 'Social Media Manager', 'angela-lopez-social-media-manager.jpg'),
+    addNewMember('Scott Estrada', 'Developer', 'scott-estrada-developer.jpg'),
+    addNewMember('Barbara Ramos', 'Graphic Designer', 'barbara-ramos-graphic-designer.jpg'),
 ];
 
-console.log(teamMembersList);
+for (let i = 0; i < teamMembersList.length; i++) {
+    for (let key in teamMembersList[i]) {
+      console.log(key + ': ' + teamMembersList[i][key]);
+    }
+    // Little temp divider for console.log
+    console.log('----------------');
+  }
+  
 
 // => Function Scheme New Member
-function addNewMember(name, surname, role, photoId) {
+function addNewMember(nameId, role, photoId) {
     const memberCard = {
-        name: name,
-        surname: surname,
-        role: role,
-        photo_Id: photoId
+        '1: Name Identifier': nameId,
+        '2: Actual Role': role,
+        '3: Photo Identifier': photoId
     };
 
     return memberCard;
