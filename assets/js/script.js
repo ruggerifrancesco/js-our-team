8fc1f69f1beae36b92ce0dc63fd736520fbe4b04
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-29 17:42:58 +0200

    removed first excercise comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,5 @@
 console.log('JS LOADED!');
 
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-
-// Wayne Barnett     ounder & CEO      wayne-barnett-founder-ceo.jpg
-// Angela Caroll     Chief Editor      angela-caroll-chief-editor.jpg
-// Walter Gordon     Office Manager    walter-gordon-office-manager.jpg
-// Angela Lopez      Social Media      Manager  angela-lopez-social-media-manager.jpg
-// Scott Estrada     Developer         scott-estrada-developer.jpg
-// Barbara Ramos     Graphic Designer  barbara-ramos-graphic-designer.jpg
-
-
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// MILESTONE 0:  ✓
-// Creare l’array di oggetti con le informazioni fornite. ✓
-// MILESTONE 1:  ✓
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto  ✓
-// MILESTONE 2:
-// Stampare le stesse informazioni su DOM sottoforma di stringhe
-
 const teamMembersList = [
     addNewMember('Wayne Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'),
     addNewMember('Angela Caroll', 'Chief Editor', 'angela-caroll-chief-editor.jpg'),
@@ -70,8 +52,8 @@ for (let i = 0; i < teamMembersList.length; i++) {
     for (let key in teamMembersList[i]) {
       console.log(key, ':', teamMembersList[i][key]);
     }
-    // Little temp divider for console.log
-    console.log('----------------');
+      // Little temp divider for console.log
+      console.log('----------------');
   }
 
   
